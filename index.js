4503b1636d93496c85dee098af9681da77fc612d
Chris Leary <learymchris@gmail.com>
2023-07-31 13:15:31 -0400

    lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const companyName = "Scuber";
+
+
+
+/* let mostProfitableNeighborhood; 
+ mostProfitableNeighborhood = "Chelsea";        also goodgit */
+
+
+let mostProfitableNeighborhood = "Chelsea";
+
+let companyCeo = "Susan Smith";
